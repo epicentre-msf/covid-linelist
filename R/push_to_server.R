ae38af3e59d853e9715d5171b7db3fdc52b3d952
Paul Campbell <pacampbell91@gmail.com>
2021-06-22 15:39:22 +0200

    add push to server scripts

---
+++ R/push_to_server.R
@@ -0,0 +1,86 @@
+# this script will create a local SQLite database with the linelist
+# compilation and aggregated data that is then pushed to the dashboard
+# server via rsync over ssh. your ssh public key is required to be in
+# .ssh/authorized_keys for both the root and epicentre user on the server
+# Patrick - your key is in both so it should work!
+
+library(DBI)
+library(tidyverse)
+library(fs)
+library(readxl)
+source(here::here("R", "push_to_server_utils.R"))
+
+shrpnt_path <- get_sharepoint_path()
+date_max <- get_prev_sunday(Sys.Date())
+data_dir <- here::here("local")
+if (!dir.exists(data_dir)) dir.create(data_dir)
+
+# Connect to local DB
+con <- DBI::dbConnect(RSQLite::SQLite(), here::here("local", "linelist.db"))
+
+# import latest linelist compilation
+df_linelist <- get_linelist_data(shrpnt_path, date_max)
+
+# write to DB
+dbWriteTable(con, "linelist", df_linelist, overwrite = TRUE)
+
+# Get weekly aggregated data
+df_weekly_aggregated <- get_agg_data(shrpnt_path, date_max)
+df_weekly_aggregated_ocba <- get_ocba_agg_data(shrpnt_path, date_max)
+
+# combine aggregated data
+df_combined <- df_linelist %>%
+  select(continent, country, country_lab, OC, site_name, date = date_master, MSF_covid_status) %>% 
+  tidyr::replace_na(list(MSF_covid_status = "Unknown")) %>% 
+  mutate(
+    date = lubridate::floor_date(lubridate::as_date(date), "week", week_start = 1), # floor date by week to monday
+    country_lab = dplyr::case_when(country_lab == "Bangladesh Camp" ~ "Bangladesh", TRUE ~ country_lab)
+  ) %>% 
+  group_by_all() %>% 
+  summarise(n = n()) %>% 
+  add_tally(wt = n, name = "Total") %>% 
+  ungroup() %>% 
+  tidyr::spread(MSF_covid_status, n, fill = 0) %>% 
+  select(-Total, everything()) %>% 
+  mutate(data_source = "Linelist") %>% 
+  bind_rows(
+    df_weekly_aggregated %>% 
+      filter(!project %in% c('Lesvos-Moria Ped'), Total > 0) %>% 
+      select(-region, -project, -week) %>% 
+      mutate(date = lubridate::as_date(date))
+  ) %>% 
+  bind_rows(df_weekly_aggregated_ocba) %>% 
+  arrange(continent, country_lab, site_name, date)
+
+# write to DB
+DBI::dbWriteTable(con, "combined", df_combined, overwrite = TRUE)
+
+# disconnect from DB
+DBI::dbDisconnect(con)
+
+# run this to send data to server
+if (FALSE) {
+  # send local database to server with rsync 
+  db_file <- here::here("local", "linelist.db")
+  host <- "epicentre@vps709766.ovh.net"
+  server_path <- "/home/epicentre/covid-linelist-dashboard"
+  system2("rsync", args = c(db_file, paste(host, fs::path(server_path, "data"), sep = ":")))
+
+  # remove cached files on server. need to ssh as root for this...
+  # if this doesn't work from R you can just do it manually on episerv:
+  # first login as root with `ssh root@vps709766.ovh.net`
+  # then run `rm -rf /home/epicentre/covid-linelist-dashboard/.rcache/*`
+  # then `exit` to logout
+  library(ssh)
+  session <- ssh_connect(host = "root@vps709766.ovh.net")
+  ssh_exec_wait(session, paste0("rm -rf ", server_path, "/.rcache/*"))
+
+  # keep the testing site up to date - don't bother with this part if it's not working from R
+  server_test_path <- "/home/epicentre/covid-linelist-dashboard-testing"
+  ssh_exec_wait(session, glue::glue("cp {server_path}/data/linelist.db {server_test_path}/data/"))
+  ssh_exec_wait(session, glue::glue("cp {server_path}/data/df_trends.rda {server_test_path}/data/"))
+  ssh_exec_wait(session, paste0("rm -rf ", server_test_path, "/.rcache/*"))
+  ssh_disconnect(session)
+}
+
+
---
+++ R/push_to_server_utils.R
@@ -0,0 +1,156 @@
+
+get_os_root <- function() {
+  OS <- Sys.info()[['sysname']]
+  dplyr::case_when(
+    OS == "Windows" ~ "D:",
+    OS == "Darwin" ~ "~"
+  )
+}
+
+get_sharepoint_path <- function() {
+  fs::path(get_os_root(), "MSF", "GRP-EPI-COVID-19 - NCoVEpi")
+}
+
+get_prev_sunday <- function(date) {
+  lubridate::floor_date(as.Date(date), unit = "week", week_start = 7)
+}
+
+pad_number <- function(x) {
+  formatC(as.numeric(x), width = 2, format = "d", flag = "0")
+}
+
+get_linelist_data <- function(shrpnt_path, date_max) {
+  path <- max(fs::dir_ls(file.path(shrpnt_path, "data", "linelist", "world"), regexp = "[.]rds$"))
+  
+  age_cut <- c(0, 5, 10, seq(15, 75, 10), Inf)
+  age_labs <- c("0-4", "5-9", "10-14", "15-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+")
+  
+  df_linelist <- readr::read_rds(path) %>% 
+    mutate_at(vars(contains("date")), lubridate::as_date) %>% 
+    mutate(
+      patcourse_presHCF = lubridate::as_date(patcourse_presHCF),
+      outcome_patcourse_presHCF = lubridate::as_date(outcome_patcourse_presHCF),
+      delay_before_consultation = MSF_date_consultation - patcourse_dateonset,
+      date_master = coalesce(MSF_date_consultation, report_date, Lab_date1, outcome_date_of_outcome, upload_date),
+      continent = countrycode::countrycode(country, "iso3c", "continent"),
+      region = countrycode::countrycode(country, "iso3c", "region23"),
+      country_lab = countrycode::countrycode(country, "iso3c", "country.name"),
+      country_lab = dplyr::case_when(
+        shape == "BGD_Camp" ~ "Bangladesh Camp",
+        country_lab == "Congo - Kinshasa" ~ "Democratic Republic of the Congo", 
+        country_lab == "Congo - Brazzaville" ~ "Republic of Congo",
+        TRUE ~ country_lab
+      ),
+      age_group = cut(age_in_years, breaks = age_cut, labels = age_labs, right = FALSE, include.lowest = TRUE),
+      age_group = forcats::fct_explicit_na(age_group, na_level = "(Unknown)")
+    ) %>% 
+    filter(date_master >= as.Date("2020-01-01") & date_master <= date_max | is.na(date_master)) %>% 
+    tidyr::replace_na(list(
+      patinfo_sex = "(Unknown)",
+      MSF_visit_type = "(Unknown)",
+      MSF_severity = "(Unknown)",
+      triage_site = "No"
+    )) %>% 
+    select(1:3, continent, region, country_lab, everything())
+  
+  df_linelist
+}
+
+get_agg_data <- function(shrpnt_path, date_max) {
+  path_agg_data <- file.path(shrpnt_path, "coordination", "Surveillance focal points coordination", "Aggregated reporting", "Report_covid_aggregate_all_v2.xlsx")
+  
+  agg_data_names <- c("sheet", "OC", "country_lab", "project", "date", "week", "Suspected", "Probable", "Confirmed", "Not a case", "Unknown")
+  
+  agg_sheets <- excel_sheets(path_agg_data)
+  agg_sheets <- agg_sheets[!grepl("sheet", agg_sheets, ignore.case = TRUE)]
+  
+  df_weekly_aggregated <- agg_sheets %>% 
+    setdiff(., c("ECU- Quito", "BGD - BD102")) %>% 
+    map_df(~{
+      print(.x)
+      oc      <- read_excel(path = path_agg_data, sheet = .x, range = "B1", col_names = FALSE) %>% pull()
+      country <- read_excel(path = path_agg_data, sheet = .x, range = "D1", col_names = FALSE) %>% pull()
+      project <- read_excel(path = path_agg_data, sheet = .x, range = "F1", col_names = FALSE) %>% pull()
+      
+      df <- read_excel(path = path_agg_data, sheet = .x, skip = 5, col_names = FALSE)
+      if(nrow(df) < 1) return(NULL)
+      
+      last_col <- ifelse(ncol(df) < 7, ncol(df), 7)
+      df %>% 
+        mutate(sheet = .x, OC = oc, country_lab = country, project = project) %>% 
+        select(sheet, OC, country_lab, project, 1:last_col)
+      
+    }) %>% 
+    set_names(agg_data_names) %>% 
+    tidyr::separate(sheet, into = c("country", "site_name"), sep = "-", extra = "merge") %>% 
+    mutate(
+      country = stringr::str_trim(country), 
+      country = case_when(country == "RCA" ~ "CAF", TRUE ~ country),
+      site_name = paste(stringr::str_trim(site_name), "(*)"), 
+      continent = countrycode::countrycode(country, "iso3c", "continent"),
+      region = countrycode::countrycode(country, "iso3c", "region23"),
+      country_lab = countrycode::countrycode(country, "iso3c", "country.name"),
+      country_lab = dplyr::case_when(
+        country_lab == "Congo - Kinshasa" ~ "Democratic Republic of the Congo", 
+        country_lab == "Congo - Brazzaville" ~ "Republic of Congo",
+        TRUE ~ country_lab),
+      date = lubridate::as_date(date),
+      Total = purrr::pmap_dbl(list(Suspected, Probable, Confirmed, `Not a case`, Unknown), sum, na.rm = TRUE),
+      data_source = "Aggregated"
+    ) %>% 
+    filter(date <= date_max) %>% 
+    select(continent, region, country, country_lab, site_name, project, everything())
+  
+  df_weekly_aggregated
+}
+
+get_ocba_agg_data <- function(shrpnt_path, date_max) {
+  ocba_dir <- fs::path(shrpnt_path, "coordination", "Surveillance focal points coordination", "Aggregated reporting", "OCBA")
+  
+  data_dict <- read_csv(fs::path(ocba_dir, "Dict_OU.csv")) %>% janitor::clean_names() 
+  # data_dict <- read_csv("data-raw/ocba_dict.csv") %>% janitor::remove_empty("cols") 
+  # write_csv(data_dict, path(ocba_dir, "Dict_OU.csv"))
+  
+  latest_data_path <- max(fs::dir_ls(ocba_dir, regexp = "COVID19_AG"))
+  df_agg_ocba <- read_csv(latest_data_path) %>% 
+    janitor::clean_names() %>% 
+    mutate(status = case_when(
+      str_detect(data, "confirmed") ~ "Confirmed",
+      str_detect(data, "suspected") ~ "Suspected",
+      TRUE ~ "Unknown"
+    )) %>% 
+    add_count(period, data, organisation_unit, wt = value, name = "Total") %>% 
+    pivot_wider(names_from = "status", values_from = "value", values_fill = 0) %>% 
+    filter(Total > 0) %>% 
+    mutate(
+      year = str_extract(period, "\\d{4}"),
+      week = str_extract(period, "W\\d+") %>% parse_number(),
+      date = aweek::week2date(glue::glue("{year}-W{pad_number(week)}")),
+      .before = 1
+    ) %>% 
+    select(-period, -year, -week, -data) %>% 
+    rename(site_name = organisation_unit) %>% 
+    left_join(select(data_dict, country = country_code_iso3, site_name = organisation_unit_level_4)) %>% 
+    mutate(
+      continent = countrycode::countrycode(country, "iso3c", "continent"),
+      # flag = countrycode::countrycode(country, "iso3c", "unicode.symbol"),
+      country_lab = countrycode::countrycode(country, "iso3c", "country.name"),
+      country_lab = dplyr::case_when(
+        country_lab == "Congo - Kinshasa" ~ "Democratic Republic of the Congo", 
+        country_lab == "Congo - Brazzaville" ~ "Republic of Congo",
+        TRUE ~ country_lab),
+      OC = "OCBA",
+      site_name = paste(stringr::str_trim(site_name), "(*)"), 
+      .before = 1
+    ) %>% 
+    mutate(data_source = "Aggregated") %>% 
+    filter(date <= date_max) %>% 
+    relocate(country, .before = country_lab) 
+  
+  df_agg_ocba
+}
+
+
+
+
+
