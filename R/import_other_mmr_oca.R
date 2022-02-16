203aa0ec891e1c19e750a8eefb75b5f1b775740d
patrickbarks <patrick.barks@gmail.com>
2022-02-16 14:18:11 -0600

    all sites now in single export file, and updates to mapping

--- R/import_other_mmr_oca.R
+++ R/import_other_mmr_oca.R
@@ -45,50 +45,30 @@ import_other_mmr_oca <- function(path_linelist_other, dict_linelist) {
     filter(map_type == "Requires derivation") %>% 
     select(var_epi, map_derive)
   
-  files_ll <- c(
-    MMR_A_HPA = llutils::list_files(
-      path_to_files,
-      pattern = "^LL_covid_dhis2_Hparkant.*\\.xls",
-      select = "latest"
-    ),
-    MMR_A_LSH = llutils::list_files(
-      path_to_files,
-      pattern = "^LL_covid_dhis2_Lashio.*\\.xls",
-      select = "latest"
-    ),
-    MMR_A_MYI = llutils::list_files(
-      path_to_files,
-      pattern = "^LL_covid_dhis2_Myitkyina.*\\.xls",
-      select = "latest"
-    ),
-    MMR_A_YNG = llutils::list_files(
-      path_to_files,
-      pattern = "^LL_covid_dhis2_Yangoon3.*\\.xls",
-      select = "latest"
-    )
+  file_ll <- llutils::list_files(
+    path_to_files,
+    pattern = "^LL_covid_dhis2_Myanmar.*\\.xls",
+    select = "latest"
   )
   
-  d_orig <- purrr::map2_dfr(
-    files_ll,
-    names(files_ll),
-    import_mmr_oca_
+  d_orig <- readxl::read_xls(
+    file_ll,
+    col_types = "text",
+    .name_repair = ~ vctrs::vec_as_names(..., repair = "unique", quiet = TRUE)
   ) %>% 
-    dplyr::left_join(dict_facilities_join, by = "site")
+    dplyr::rename_with(hmatch::string_std) %>% 
+    janitor::remove_empty("rows") %>% 
+    dplyr::mutate(
+      upload_date = as.character(llutils::extract_date(file_ll)),
+    ) %>% 
+    rename(uid = organisation_unit) %>% 
+    left_join(dict_facilities_join, by = "uid")
+  
   
   ### Check for unseen values in derivation variables
   test_set_equal(
     d_orig$sex,
     c("[f] female", "[m] male", NA)
-  )
-  test_set_equal(
-    d_orig$confirmation_status_at_exit,
-    c(
-      NA,
-      "[cpv] confirmed case (test positive)",
-      "[pnd] probable (test not done)",
-      "[pin] probable (test inconclusive)",
-      "[sng] suspect (test negative)"
-    )
   )
   test_set_equal(
     d_orig$pregnant,
@@ -102,7 +82,12 @@ import_other_mmr_oca <- function(path_linelist_other, dict_linelist) {
   )
   test_set_equal(
     d_orig$malaria_rdt_at_admission,
-    c("[nd] not done", "[n] no", NA)
+    c(
+      "[nd] not done",
+      "[n] no",
+      "[n] not currently or recently pregnant", # currently N = 89, should followup
+      NA
+    )
   )
   test_set_equal(
     d_orig$received_oxygen_therapy,
@@ -120,21 +105,55 @@ import_other_mmr_oca <- function(path_linelist_other, dict_linelist) {
       "[rf] external msf referral / transfer",
       "[tr] internal msf transfer",
       "[la] left against medical advice",
-      "[dh] discharged home", "[dd] dead"
+      "[dh] discharged home", "[dd] dead",
+      "[dd] developmental delay/disorder" ## ??
     )
   )
   test_set_equal(
     d_orig$nutrition_status_at_admission,
-    c("[mam] moderate acute malnutrition", "[nm] not malnourished", NA)
+    c(
+      "[mam] moderate acute malnutrition",
+      "[nm] not malnourished",
+      NA
+    )
   )
+  
+  allowed_diagnosis <- c(
+    "[HRI] HIV and HIV-related illnesses",
+    "[CCC] Chronic cardio & cerebrovascular disease",
+    "[AHC] Acute hypertensive crisis",
+    "[CCD] Chronic complications of diabetes",
+    "[CKD] Chronic kidney disease",
+    "[MLG] Malignancy",
+    "[CRD] Non-infectious respiratory diseases - chronic",
+    "[PTB] Tuberculosis, pulmonary",
+    "[ETB] Tuberculosis, extra pulmonary",
+    "[MSL] Measles",
+    "[SAM] Severe acute malnutrition",
+    "[LRT] Lower respiratory tract infection",
+    "[NHT] Neonatal hypothermia",
+    NA
+  )
+  test_set_equal(
+    d_orig$diagnosis_at_exit_primary,
+    allowed_diagnosis
+  )
+  test_set_equal(
+    d_orig$diagnosis_at_exit_secondary,
+    allowed_diagnosis
+  )
+  
   
   # d_orig %>% 
   #   count(inpatient_exit_status)
   # 
   # dict_factors %>%
-  #   filter(variable == "MSF_covid_status") %>%
+  #   filter(variable == "MSF_measles") %>%
   #   select(1:2) %>%
   #   print(n = "all")
+  # 
+  # d_orig %>% 
+  #   count(diagnosis_at_exit_primary)
   
   
   ### Derived variables
@@ -159,12 +178,6 @@ import_other_mmr_oca <- function(path_linelist_other, dict_linelist) {
         site %in% "MMR_A_LSH" ~ "Shan",
         site %in% "MMR_A_MYI" ~ "Kachin",
         site %in% "MMR_A_YNG" ~ "Yangoon"
-      ),
-      MSF_covid_status = case_when(
-        confirmation_status_at_exit %in% "[CPV] Confirmed case (test positive)" ~ "Confirmed",
-        confirmation_status_at_exit %in% "[PIN] Probable (test inconclusive)" ~ "Probable",
-        confirmation_status_at_exit %in% "[PND] Probable (test not done)" ~ "Probable",
-        confirmation_status_at_exit %in% "[SNG] Suspect (test negative)" ~ "Suspected"
       ),
       Comcond_preg = case_when(
         pregnant %in% "[N] No" ~ "No",
@@ -199,6 +212,58 @@ import_other_mmr_oca <- function(path_linelist_other, dict_linelist) {
         inpatient_exit_status %in% "[OT] Other" ~ "Other",
         inpatient_exit_status %in% "[TR] Internal MSF transfer" ~ "Transferred",
         inpatient_exit_status %in% "[RF] External MSF referral / transfer" ~ "Transferred"
+      ),
+      MSF_hiv_status = case_when(
+        diagnosis_at_exit_primary %in% "[HRI] HIV and HIV-related illnesses" |
+          diagnosis_at_exit_secondary %in% "[HRI] HIV and HIV-related illnesses" ~ "Positive (unknown)",
+        TRUE ~ NA_character_
+      ),
+      Comcond_cardi = case_when(
+        diagnosis_at_exit_primary %in% "[CCC] Chronic cardio & cerebrovascular disease" |
+          diagnosis_at_exit_secondary %in% "[CCC] Chronic cardio & cerebrovascular disease" ~ "Yes",
+        TRUE ~ NA_character_
+      ),
+      MSF_hypertension = case_when(
+        diagnosis_at_exit_primary %in% "[AHC] Acute hypertensive crisis" |
+          diagnosis_at_exit_secondary %in% "[AHC] Acute hypertensive crisis" ~ "Yes",
+        TRUE ~ NA_character_
+      ),
+      Comcond_diabetes = case_when(
+        diagnosis_at_exit_primary %in% "[CCD] Chronic complications of diabetes" |
+          diagnosis_at_exit_secondary %in% "[CCD] Chronic complications of diabetes" ~ "Yes",
+        TRUE ~ NA_character_
+      ),
+      Comcond_renal = case_when(
+        diagnosis_at_exit_primary %in% "[CKD] Chronic kidney disease" |
+          diagnosis_at_exit_secondary %in% "[CKD] Chronic kidney disease" ~ "Yes",
+        TRUE ~ NA_character_
+      ),
+      Comcond_malig = case_when(
+        diagnosis_at_exit_primary %in% "[MLG] Malignancy" |
+          diagnosis_at_exit_secondary %in% "[MLG] Malignancy" ~ "Yes",
+        TRUE ~ NA_character_
+      ),
+      Comcond_lung = case_when(
+        diagnosis_at_exit_primary %in% "[CRD] Non-infectious respiratory diseases - chronic" |
+          diagnosis_at_exit_secondary %in% "[CRD] Non-infectious respiratory diseases - chronic" ~ "Yes",
+        TRUE ~ NA_character_
+      ),
+      MSF_tb_active = case_when(
+        diagnosis_at_exit_primary %in% c("[PTB] Tuberculosis, pulmonary", "[ETB] Tuberculosis, extra pulmonary") |
+          diagnosis_at_exit_secondary %in% c("[PTB] Tuberculosis, pulmonary", "[ETB] Tuberculosis, extra pulmonary") ~ "Yes (unknown)",
+        TRUE ~ NA_character_
+      ),
+      MSF_tb_type = case_when(
+        diagnosis_at_exit_primary %in% "[PTB] Tuberculosis, pulmonary" |
+          diagnosis_at_exit_secondary %in% "[PTB] Tuberculosis, pulmonary" ~ "Pulmonary",
+        diagnosis_at_exit_primary %in% "[ETB] Tuberculosis, extra pulmonary" |
+          diagnosis_at_exit_secondary %in% "[ETB] Tuberculosis, extra pulmonary" ~ "Extrapulmonary",
+        TRUE ~ NA_character_
+      ),
+      MSF_measles = case_when(
+        diagnosis_at_exit_primary %in% "[MSL] Measles" |
+          diagnosis_at_exit_secondary %in% "[MSL] Measles" ~ "Yes",
+        TRUE ~ NA_character_
       ),
       MSF_malnutrition = case_when(
         nutrition_status_at_admission %in% "[MAM] Moderate acute malnutrition" ~ "Yes",
@@ -270,19 +335,3 @@ import_other_mmr_oca <- function(path_linelist_other, dict_linelist) {
   dplyr::select(df_data, all_of(cols_derive), all_of(ll_template), starts_with("extra_"))
 }
 
-
-
-import_mmr_oca_ <- function(path, site) {
-  readxl::read_xls(
-    path,
-    col_types = "text",
-    .name_repair = ~ vctrs::vec_as_names(..., repair = "unique", quiet = TRUE)
-  ) %>% 
-    dplyr::rename_with(hmatch::string_std) %>% 
-    janitor::remove_empty("rows") %>% 
-    dplyr::mutate(
-      linelist_row = 1:n(),
-      upload_date = as.character(llutils::extract_date(path)),
-      site = site
-    )
-}
